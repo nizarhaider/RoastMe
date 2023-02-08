8bfa945d1969355bfc0a4f570325cefbb2a0f23f
Nizar <nizarhaider@gmail.com>
2023-02-08 08:51:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    folder = '/app/static/images'
+    # folder = '/app/static/images'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    folder = os.path.join(basedir, "static", "images")
     shutil.rmtree(folder)
     os.mkdir(folder)
 
@@ -29,15 +31,16 @@ def index():
 
 @app.route('/', methods=['POST'])
 def handle_form(): 
-    
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
     df= pd.read_csv('data_with_embeddings_fixed.csv')
     df["embeddings"] = df["embeddings"].apply(lambda x: np.array(list(map(float, x.replace("[", "").replace("]", "").split()))))
-    
+    base_image =  os.path.join(basedir, "static", "images")
     image_file = request.files['image']
-    image_file.save(os.path.join('app\static\images', image_file.filename))
+    image_file.save(os.path.join(base_image, image_file.filename))
 
     # image = request.files.get('image')
-    user_img_path = os.path.join('app\static\images', image_file.filename)
+    user_img_path = os.path.join(base_image, image_file.filename)
     if imghdr.what(user_img_path) == None:
         return jsonify({"fun_pass": "Invalid image type. I only support png, jpg or jpeg at the moment :/ "})
 
@@ -51,8 +54,8 @@ def handle_form():
 
         comments, match_img = result
         comments = ast.literal_eval(comments)
-        match_img.save(os.path.join('app/static/images', "match.jpg"))
-        match_img_path = os.path.join('app/static/images', "match.jpg")
+        match_img.save(os.path.join(base_image, "match.jpg"))
+        match_img_path = os.path.join(base_image, "match.jpg")
         # user_img_path = os.path.join('static/images', "user.jpg")
         comments=json.dumps(comments)
         response = {'comments': comments, 'match_img': match_img_path, 'user_img': user_img_path, "fun_pass": fun_pass}

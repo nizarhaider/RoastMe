b38439d8e15b278ce7835cc237f8a91aaa2f8a34
Nizar <nizarhaider@gmail.com>
2023-04-07 21:56:22 +0530

    Update scrapper copy.py

--- others/data_collection_and_preprocessing/scrapper copy.py
+++ others/data_collection_and_preprocessing/scrapper copy.py
@@ -6,11 +6,11 @@ import concurrent.futures
 
 
 reddit = praw.Reddit(
-    client_id="m_zO2sVNL0uFZ-5U2grDVg",
-    client_secret="O11TNu-7b-obob1c_i7q2-3WeBfZag",
-    username="2broke2code",
-    password=password,
-    user_agent="android:com.2broke2code.roastbot:v1.2.1 (by u/2broke2code)",
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    username=USERNAME,
+    password=PASSWORD,
+    user_agent=USER_AGENT,
 )
 
 all_ids = []

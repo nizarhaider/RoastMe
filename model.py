d60499e8de33db73eb81807dfeee649fd16b16b4
Nizar <43636043+nizarhaider@users.noreply.github.com>
2023-09-17 12:18:33 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import urllib.request
 from PIL import Image
-from mtcnn import MTCNN
-from facenet_pytorch import InceptionResnetV1
+from facenet_pytorch import MTCNN, InceptionResnetV1
 import numpy as np
 import cv2
 import torch
@@ -9,39 +8,32 @@ from scipy.spatial import distance
 
 model = InceptionResnetV1(pretrained='vggface2').eval()
 detector = MTCNN()
-image_size=(256,400)
+image_size = (256, 400)
 
-def find_closest_match(df, path,  threshold_norm=0.7):
+def find_closest_match(df, path, threshold_norm=0.7):
     try:
         # Load the image
         image = cv2.imread(path)
-        # image = Image.open(io.BytesIO(image.read()))
-        # Convert PIL.Image to numpy.ndarray
-        img = np.array(image)
-        # print(img.shape)
-        # Change image color from RGB to BGR
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # Convert the image color from BGR to RGB
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # Detect the face in the image
-        face = detector.detect_faces(img)[0]
+        boxes, probs = detector.detect(img)
+        if boxes is None:
+            raise Exception("No face detected")
+        
+        # Use the first detected face
+        box = boxes[0].astype(int)
         # Crop the face from the image
-        face_cropped = img[face['box'][1]:face['box'][1] + face['box'][3], face['box'][0]:face['box'][0]+face['box'][2]]
-        # Convert PIL.Image to numpy.ndarray
-        face_cropped = np.array(face_cropped)
-        # Change image color from RGB to BGR
-        face_cropped = cv2.cvtColor(face_cropped, cv2.COLOR_RGB2BGR)
+        face_cropped = img[box[1]:box[3], box[0]:box[2]]
+        
         # Resize the face_cropped tensor to match the expected input size of the model
         face_cropped = cv2.resize(face_cropped, (160, 160)).astype(float)
-        # Reorder dimensions of your image data so it is in HWC format
-        face_cropped = face_cropped.transpose((2, 0, 1))
         # Normalize the pixel values to be between -1 and 1
         face_cropped = (face_cropped / 255) * 2 - 1
-        # Convert numpy.ndarray to tensor
-        face_cropped = torch.from_numpy(face_cropped)
+        # Convert the face to tensor
+        face_cropped = torch.tensor(face_cropped).permute(2, 0, 1).unsqueeze(0).float()
         # Extract the facial embeddings
-        face_cropped = face_cropped.float()
-        face_cropped = torch.unsqueeze(face_cropped, 0)
         face_embedding = model(face_cropped)[0]
-        # print(face_embedding.detach().numpy())
         print("Generated Embeddings")
         
         # Define the distance function
@@ -50,36 +42,28 @@ def find_closest_match(df, path,  threshold_norm=0.7):
                 return distance.euclidean(x, face_embedding.detach().numpy())
             else:
                 return None
+        
         # Apply the distance function to the 'embeddings' column of the dataframe
         df['distance'] = df['embeddings'].apply(find_distance)
         # Normalize the distance
         df['distance_norm'] = df['distance'] / df['distance'].max()
-        # print(df['distance_norm'])
         # Find the index of the closest match
         closest_index = df['distance_norm'].idxmin()
-        # print("Similarity level: ", df.at[closest_index, 'distance_norm'])
         
         if df.at[closest_index, 'distance_norm'] < threshold_norm:
-            
             # Return the 'comments' column of the closest match
             url = df.at[closest_index, 'image_url']
             image = Image.open(urllib.request.urlretrieve(url)[0])
             comments = df.at[closest_index, 'comments']
-            
             fun_pass = "True"
             result = comments, image
-            
             return fun_pass, result
-        
         else:
             fun_pass = "Similarity not found"
             result = 0, 0
-            
             return fun_pass, result
+
     except:
-
-        fun_pass = "Cannot detect face" 
+        fun_pass = "Cannot detect face"
         result = 0, 0
-
         return fun_pass, result
-    
